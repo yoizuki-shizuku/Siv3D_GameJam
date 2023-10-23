3a533c7c136134f7739e12c53a4d752ec324ffb4
NakamuraRyo0304 <shino.nakiri@gmail.com>
2023-10-24 01:08:05 +0900

    DrawTextureクラスに設定された画像からポリゴンを取得する機能を追加

--- Libraries/Nakamura/DrawTexture.hpp
+++ Libraries/Nakamura/DrawTexture.hpp
@@ -11,6 +11,12 @@ private:
 
 	// テクスチャの配列 // キー・テクスチャパス
 	std::map<const char32_t*, Texture> m_textures;
+	std::map<const char32_t*, Image> m_images;
+
+private:
+
+	// ポリゴン割り
+	const double MAX_DISTANCE = 5.0;
 
 public:
 
@@ -18,11 +24,41 @@ public:
 	DrawTexture();
 	~DrawTexture();
 
-	// Add関数
+	/// <summary>
+	/// テクスチャを追加する関数
+	/// </summary>
+	/// <param name="key">登録キーの文字列(U"文字列")</param>
+	/// <param name="path">ファイルパスの文字列</param>
+	/// <returns>なし</returns>
 	void AddTexture(const char32_t* key, const char32_t* path);
 
-	// Draw関数
+	/// <summary>
+	/// 登録したテクスチャを描画する関数
+	/// </summary>
+	/// <param name="key">登録キーの文字列(U"文字列")</param>
+	/// <param name="pos">描画する位置</param>
+	/// <param name="scale">拡大率(デフォルトは1.0)</param>
+	/// <param name="rotate">回転率(デフォルトは0.0)</param>
+	/// <param name="mirrored">左右反転フラグ(デフォルトはfalse)</param>
+	/// <param name="flipped">上下反転フラグ(デフォルトはfalse)</param>
+	/// <returns>なし</returns>
 	void Draw(const char32_t* key, const Vec2& pos, const double& scale = 1.0, const double& rotate = 0.0,
 		const bool& mirrored = false, const bool& flipped = false);
+
+	/// <summary>
+	/// イメージを返す関数
+	/// </summary>
+	/// <param name="key">登録キーの文字列(U"文字列")</param>
+	/// <returns>イメージ</returns>
+	const Image& GetImage(const char32_t* key);
+
+	/// <summary>
+	/// ポリゴンゲッター
+	/// </summary>
+	/// <param name="key">登録キーの文字列(U"文字列")</param>
+	/// <param name="show">ポリゴンの可視化</param>
+	/// <param name="color">Gizmoの色</param>
+	/// <returns>テクスチャのポリゴン判定</returns>
+	const Polygon& GetPolygon(const char32_t* key, const bool& show = false, const ColorF& color = Palette::Yellow);
 
 };
---
+++ Scene/RDebugScene/RDebugScene.cpp
@@ -0,0 +1,38 @@
+﻿/*
+ *	@File	RDebugScene.cpp
+ *	@Brief	中村のデバッグシーン(最終的に消す)。
+ *	@Date	2023-10-23
+ *  @Author NakamuraRyo
+ */
+
+#include "RDebugScene.h"
+#include "../../Libraries/Nakamura/DrawTexture.hpp"
+
+RDebugScene::RDebugScene() :
+	MyClass::Scene(),m_pos{640,360}
+{
+}
+
+void RDebugScene::Initialize()
+{
+	m_tex = std::make_unique<DrawTexture>();
+	m_tex->AddTexture(U"Stage", U"../Resources/Textures/Stage.png");
+}
+
+void RDebugScene::Update()
+{
+}
+
+void RDebugScene::Render()
+{
+
+	// ポリゴンの取得と描画
+	m_tex->GetPolygon(U"Stage",true);
+
+	// ステージを表示
+	m_tex->Draw(U"Stage", m_pos);
+}
+
+void RDebugScene::Finalize()
+{
+}
