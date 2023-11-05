746d90e8fea8e752d938c8d6e91b1d1e84d5e35e
ながせまるかずま <pokarukusarudo@gamil.com>
2023-11-05 10:36:53 +0900

    リザルト表示追加

--- Scene/PlayScene/Prize/PrizeManager.cpp
+++ Scene/PlayScene/Prize/PrizeManager.cpp
@@ -9,6 +9,10 @@
 #include "../Effect/AcquisitionEffect.h"
 
 #include "../../../Libraries/Nakamura/DrawTexture.hpp"
+#include "../../../Libraries/Yamamoto/Numeral.h"
+
+#define ANIM_VERTICAL	450
+#define ANIM_BESIDE		750
 
 PrizeManager::PrizeManager()
 {
@@ -23,6 +27,9 @@ void PrizeManager::Initialize(P2World world)
 	m_score = 0;
 
 	m_drawTexture = std::make_unique<DrawTexture>();
+
+	m_craneCount = std::make_unique<Numeral>();
+
 
 	//U"../Resources/Textures/CraneBody.png"
 	m_drawTexture->AddTexture(U"Small",U"../Resources/Textures/SmallPrize.png");
@@ -60,8 +67,11 @@ void PrizeManager::Update()
 		// 規定値よりも下に行ったら消す
 		if (m_prizes[i]->GetPos().y >= 800)
 		{
-			//constexpr RectF shape{ 100, -100, 700, 0 };
-			//prizes->SetPos(RandomVec2(shape));
+			if (m_prizes[i]->GetName() == U"Rescue")
+			{
+				m_rescueFlag = true;
+			}
+
 
 			m_score += m_prizes[i]->GetScore();
 			m_effect.add<AcquisitionEffect>(Vec2(1050, 730));
@@ -83,6 +93,50 @@ void PrizeManager::Render()
 		m_drawTexture->Draw(prizes->GetName());
 	}
 
+}
+
+void PrizeManager::Render_Result()
+{
+
+	int sceneHalfSizeX = (Scene::Size() / 2).x;
+	int sceneHalfSizeY = (Scene::Size() / 2).y;
+
+	// 横に伸ばす
+	m_animationTime_Beside += Scene::DeltaTime() * 0.7f;
+
+	// 縦に伸ばす
+	m_animationTime_Vertical += m_animationTime_Beside >= 1.0f ? Scene::DeltaTime() : 0;
+
+	// アニメーション
+	m_animationTime_Rescue += m_animationTime_Vertical >= 1.0f ? Scene::DeltaTime() : 0;
+
+	//　上限下限値設定
+	m_animationTime_Beside = Clamp(m_animationTime_Beside, 0.0f, 1.0f);
+	m_animationTime_Vertical = Clamp(m_animationTime_Vertical, 0.0f, 1.0f);
+	m_animationTime_Rescue = Clamp(m_animationTime_Rescue, 0.0f, 1.0f);
+
+	double beside = Easing::Circ(m_animationTime_Beside) * ANIM_BESIDE;
+	double vertical = Easing::Quad(m_animationTime_Vertical) * ANIM_VERTICAL;
+
+	// 矩形描画
+	Rect{ Arg::center(sceneHalfSizeX, sceneHalfSizeY) ,
+		  2 + (int)beside,
+		  2 + (int)vertical }.draw(Palette::Gray);
+
+	// 数字描画
+	if (m_animationTime_Vertical >= 1.0f)
+	{
+		m_craneCount->SetPosition(Vec2(sceneHalfSizeX, sceneHalfSizeY - 200));
+		m_craneCount->SetNumber(m_score);
+		m_craneCount->Render();
+	}
+
+	// レスキュー対象描画
+	if (m_rescueFlag)
+	{
+		m_drawTexture->SetTexInfo(U"Rescue", Vec2(sceneHalfSizeX, sceneHalfSizeY + 100), m_animationTime_Rescue);
+		m_drawTexture->Draw(U"Rescue");
+	}
 }
 
 void PrizeManager::CreatePrize(IPrizeFactory* prize, P2World world,int num, int pulsPosY)
