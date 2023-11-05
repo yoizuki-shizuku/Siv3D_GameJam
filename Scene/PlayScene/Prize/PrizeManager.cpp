b2ff3e4a8fa7f78cf805aff960364e452f9989da
NakamuraRyo0304 <shino.nakiri@gmail.com>
2023-11-05 09:43:02 +0900

    PrizeManagerの範囲外アクセスのエラーを範囲for->for<sizeで解消

--- Scene/PlayScene/Prize/PrizeManager.cpp
+++ Scene/PlayScene/Prize/PrizeManager.cpp
@@ -48,32 +48,28 @@ void PrizeManager::Update()
 {
 	m_effect.update();
 
-	for (auto& prizes : m_prizes)
+	// for(auto& prizes : m_prizes)
+	for (size_t i = 0; i < m_prizes.size(); ++i)
 	{
 		// 上限設定
-		if (prizes->GetPos().x >= 1190)
+		if (m_prizes[i]->GetPos().x >= 1190)
 		{
-			prizes->SetPos(Vec2(1190, prizes->GetPos().y));
+			m_prizes[i]->SetPos(Vec2(1190, m_prizes[i]->GetPos().y));
 		}
 
 		// 規定値よりも下に行ったら消す
-		if (prizes->GetPos().y >= 800)
+		if (m_prizes[i]->GetPos().y >= 800)
 		{
 			//constexpr RectF shape{ 100, -100, 700, 0 };
 			//prizes->SetPos(RandomVec2(shape));
 
-			m_score += prizes->GetScore();
+			m_score += m_prizes[i]->GetScore();
 			m_effect.add<AcquisitionEffect>(Vec2(1050, 730));
 
-			m_prizes.remove(prizes);
+			m_prizes.remove(m_prizes[i]);
 
 		}
-
 	}
-
-
-
-
 }
 
 void PrizeManager::Render()
